6691b5d8bcdffb5463807bd31c4042e99b4a6fcf
enaoi <charlotte.xuuuu@gmail.com>
2020-09-30 23:19:44 +0800

    update

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  settings: {
+    "import/resolver": {
+      alias : {
+        map: [
+          ["@","./src"]
+        ],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    }
+  },
+}
